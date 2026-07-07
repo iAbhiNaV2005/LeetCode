99149e2b9ed4811aaa65ded96ba022778b8cae42
Abhinav <meabhinav2005@gmail.com>
2026-07-07 11:19:51 +0530

    [503] [Time Beats: 61.11%] [Memory Beats: 47.80%] - LeetPush

--- 503-Next-Greater-Element-II.java
+++ 503-Next-Greater-Element-II.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int[] result = new int[n];
+        int[] ans = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            result[i] = -1;
+        Stack<Integer> stack = new Stack<>();
 
-            int j = (i + 1) % n;
-
-            while (j != i) {
-                if (nums[j] > nums[i]) {
-                    result[i] = nums[j];
-                    break;
-                }
-
-                j = (j + 1) % n;
+        for(int i = 2*n - 1; i>= 0 ; i--){
+            int curr = nums[i%n];
+            while(!stack.isEmpty() && stack.peek() <= curr){
+                stack.pop();
             }
+            if(i < n){
+                ans[i] = stack.isEmpty()? -1 : stack.peek();
+            }
+
+            stack.push(curr);
         }
 
-        return result;
+        return ans;
     }
 }
